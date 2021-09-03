eafbf643e9c8af7098331d883dd38dac89f565b6
ulfetalhussaini <ulfetalhussaini@yahoo.com>
2021-09-03 20:53:15 +0300

    update

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,7 @@ import MovieCard from "./MovieCard";
 
 function App() {
   const title = "Mad Max";
-  const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
+ // const posterURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
   return (
